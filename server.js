2456db56f5020e0d827005ce476ebac3a7645e06
unknown <shivakumar1404m@gmail.com>
2025-02-03 15:52:16 +0530

    Added missings

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ Thank you for choosing LaundryBoy!`,
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
